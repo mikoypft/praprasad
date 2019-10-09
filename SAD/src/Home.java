e80f57dd78bfe1bc00e8c2aae7e819a5385a53ae
mikoypft <54622861+mikoypft@users.noreply.github.com>
2019-10-09 18:11:08 +0800

    PRA

--- SAD/src/Home.java
+++ SAD/src/Home.java
@@ -15,10 +15,17 @@ import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import javax.swing.JInternalFrame;
 import javax.swing.JTextField;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.border.MatteBorder;
+import java.time.Month;
 
 public class Home extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -144,6 +151,87 @@ public class Home extends JFrame {
 		lblNewLabel.setOpaque(true);
 		lblNewLabel.setBounds(0, 0, 225, 661);
 		contentPane.add(lblNewLabel);
+		
+		
+		JPanel basinf = new JPanel();
+		basinf.setBackground(new Color(255, 255, 255));
+		basinf.setBounds(225, 0, 759, 661);
+		contentPane.add(basinf);
+		basinf.setLayout(null);
+		
+		
+		JLabel lblPatientsBasicInformation = new JLabel("PATIENT'S BASIC INFORMATION");
+		lblPatientsBasicInformation.setFont(new Font("Tahoma", Font.BOLD, 30));
+		lblPatientsBasicInformation.setBounds(50, 100, 600, 25);
+		basinf.add(lblPatientsBasicInformation);
+		
+		JLabel lblName = new JLabel("NAME");
+		lblName.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblName.setBounds(50, 200, 46, 14);
+		basinf.add(lblName);
+		
+		JLabel lblDateOfBirth = new JLabel("DATE OF BIRTH");
+		lblDateOfBirth.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblDateOfBirth.setBounds(50, 250, 125, 15);
+		basinf.add(lblDateOfBirth);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(Month.values()));
+		comboBox.setBounds(175, 245, 100, 25);
+		basinf.add(comboBox);
+		
+		JLabel lblSex = new JLabel("SEX");
+		lblSex.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblSex.setBounds(50, 300, 46, 14);
+		basinf.add(lblSex);
+		
+		JLabel lblHomeAddress = new JLabel("HOME ADDRESS");
+		lblHomeAddress.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblHomeAddress.setForeground(new Color(0, 0, 0));
+		lblHomeAddress.setBounds(50, 350, 125, 15);
+		basinf.add(lblHomeAddress);
+		
+		JLabel lblProvincialAddress = new JLabel("PROVINCIAL ADDRESS");
+		lblProvincialAddress.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblProvincialAddress.setBounds(50, 400, 175, 15);
+		basinf.add(lblProvincialAddress);
+		
+		JLabel lblContactNumbers = new JLabel("CONTACT NUMBERS:");
+		lblContactNumbers.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblContactNumbers.setBounds(50, 450, 175, 15);
+		basinf.add(lblContactNumbers);
+		
+		JLabel lblNewLabel_4 = new JLabel("HOME:");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.ITALIC, 15));
+		lblNewLabel_4.setBounds(75, 500, 50, 15);
+		basinf.add(lblNewLabel_4);
+		
+		JLabel lblOffice = new JLabel("OFFICE:");
+		lblOffice.setFont(new Font("Tahoma", Font.ITALIC, 15));
+		lblOffice.setBounds(75, 550, 75, 15);
+		basinf.add(lblOffice);
+		
+		JLabel lblMobile = new JLabel("MOBILE:");
+		lblMobile.setFont(new Font("Tahoma", Font.ITALIC, 15));
+		lblMobile.setBounds(75, 600, 75, 15);
+		basinf.add(lblMobile);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.BOLD, 15));
+		textField.setBorder(new MatteBorder(0, 0, 2, 0, (Color) new Color(0, 206, 209)));
+		textField.setBounds(125, 195, 500, 25);
+		basinf.add(textField);
+		textField.setColumns(10);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
+		comboBox_1.setBounds(300, 245, 100, 25);
+		basinf.add(comboBox_1);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001", "2000", "1999", "1998", "1997", "1996", "1995", "1994", "1993", "1992", "1991", "1990", "1989", "1988", "1987", "1986", "1985", "1984", "1983", "1982", "1981", "1980", "1979", "1978", "1977", "1976", "1975", "1974", "1973", "1972", "1971", "1970", "1969", "1968", "1967", "1966", "1965", "1964", "1963", "1962", "1961", "1960", "1959", "1958", "1957", "1956", "1955", "1954", "1953", "1952", "1951", "1950", "1949", "1948", "1947", "1946", "1945", "1944", "1943", "1942", "1941", "1940", "1939", "1938", "1937", "1936", "1935", "1934", "1933", "1932", "1931", "1930", "1929", "1928", "1927", "1926", "1925", "1924", "1923", "1922", "1921", "1920", "1919"}));
+		comboBox_2.setBounds(425, 245, 100, 25);
+		basinf.add(comboBox_2);
 		
 		JPanel patrec = new JPanel();
 		patrec.setBackground(new Color(220, 220, 220));
